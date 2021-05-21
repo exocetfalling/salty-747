cd04cad0e9ac6cde4604a6b4bfe5a667c2b6ab31
747-4EVER <71572892+747-4EVER@users.noreply.github.com>
2021-05-21 15:07:25 +0200

    First Commit to enable CHKL Button

---
+++ salty-747/html_ui/Pages/VCockpit/Instruments/Airliners/B747_8/EICAS/Pages/B747_8_LowerEICAS_ECL.js
@@ -0,0 +1,23 @@
+var B747_8_LowerEICAS_ECL;
+(function (B747_8_LowerEICAS_ECL) {
+    class Display extends Airliners.EICASTemplateElement {
+        constructor() {
+            super();
+            this.isInitialised = false;
+        }
+        get templateID() { return "B747_8LowerEICASECLTemplate" }
+        connectedCallback() {
+            super.connectedCallback();
+            TemplateElement.call(this, this.init.bind(this));
+        }
+        init() {
+            this.isInitialised = true;
+        }
+        update(_deltaTime) {
+
+        }
+    }
+    B747_8_LowerEICAS_ECL.Display = Display;
+})(B747_8_LowerEICAS_ECL || (B747_8_LowerEICAS_ECL = {}));
+customElements.define("b747-8-lower-eicas-ecl", B747_8_LowerEICAS_ECL.Display);
+//# sourceMappingURL=B747_8_LowerEICAS_ECL.js.map
